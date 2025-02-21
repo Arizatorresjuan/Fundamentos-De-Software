1e60d3a60a5324bc91c2e4144d4aa31421607f9b
Arizatorresjuan <arizatorresjuan@gmail.com>
2025-02-21 15:51:25 -0500

    Add files via upload

---
+++ Ejercicio_Preliminar.m
@@ -0,0 +1,16 @@
+A = magic(4);
+B = rand(3,5,2);
+ "temperature is " + c + " Celsius" ;
+ A = ["a","bb","ccc"; "dddd","eeeeee","fffffff"];
+ strlength(A)
+ A = [1 3 5];
+max(A)
+B = [3 6 9];
+union(A,B);
+disp("hello world") 
+x = linspace(0,2*pi);
+y = sin(x);
+plot(x,y)
+xlabel("x")
+ylabel("sin(x)")
+title("Plot of the Sine Function")
